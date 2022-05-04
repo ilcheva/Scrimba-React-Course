db2b5f7b0db1986c052286a3756b738cc3b3fb26
Simona Ilcheva <s.ilcheva@gmail.com>
2022-05-04 13:49:24 +0300

    main component

--- react-facts-app/src/App.js
+++ react-facts-app/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import NavBar from './Components/NavBar';
+import Main from './Components/Main';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Main />
     </div>
   );
 }
---
+++ react-facts-app/src/Components/Main.js
@@ -0,0 +1,23 @@
+import logo from '../logo.svg';
+
+
+function Main() {
+    return (
+        <div className="main">
+            <h1 className="main-title">Fun Facts about React</h1>
+            <div className="text">
+                <ul className="facts">
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100K stars on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+                <img src={logo} className="transperant-logo" alt="logo" />
+            </div>
+
+
+        </div>
+    )
+}
+export default Main;
